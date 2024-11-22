cfcf945401d0c8196a62d039c36d5a7fb2497490
macmac <donmacnino@gmail.com>
2024-11-22 15:29:59 +0800

    build:try

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,4 @@
 const { getDefaultConfig } = require("expo/metro-config");
-console.log('BACKEND_URL: ',process.env.BACKEND_URL)
-console.log('NOTIFICATION_SOCKET: ',process.env.NOTIFICATION_SOCKET)
-console.log('CHAT_SOCKET: ',process.env.CHAT_SOCKET)
 
 
 const config = getDefaultConfig(__dirname);
